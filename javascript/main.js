17c5e416c2fd231b4dcdacf5e11980eb6804b08b
micheleferri01 <micheleferri095@gmail.com>
2026-01-13 18:06:23 +0100

    milestone 2

--- javascript/main.js
+++ javascript/main.js
@@ -1,38 +1,57 @@
 const ticketForm = document.getElementById("ticket-form");
-const kil = document.getElementById("chilometri");
-const age = document.getElementById("age");
+const kilInput = document.getElementById("kilometri");
+const ageInput = document.getElementById("age");
 
-ticketForm.addEventListener("submit", (e) =>{
+ticketForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log(`Kilometri da percorrere: ${kil.value} Km`);
-    console.log("Età:",age.value);
+    let kil = kilInput.value;
+    let age = ageInput.value;
+    console.log(typeof kil);
+    console.log(`Kilometri da percorrere: ${kil} Km`);
+    console.log("Età:", age);
     const priceForUser = price();
-    console.log(`Prezzo Biglietto: ${priceForUser} €`)
-    kil.value = "";
-    age.value = "";
-    
+    console.log(`Prezzo Biglietto: ${priceForUser} €`);
+    const ticket = document.getElementById("ticket");
+    const Kilometri = document.getElementById("km");
+    const ageCategory = document.getElementById("age-category");
+    const finalPrice = document.getElementById("prezzo");
+
+    if(!kil || !age){
+        ticket.classList.add("d-none");
+        return;
+    }
+
+    Kilometri.innerText = `${kil} Km`;
+    ageCategory.innerText = `${age}`;
+    finalPrice.innerText = `${priceForUser} €`;
+    ticket.classList.remove("d-none");
+
+    kilInput.value = "";
+    ageInput.value = "";
+
 });
 
-function price(){
+function price() {
     const pricePerKil = 0.21;
-    const basePrice = (pricePerKil * kil.value).toFixed(2);
-    if(age.value === "minorenne"){
-       return discountForMinor(basePrice);
+    const basePrice = (pricePerKil * kilInput.value).toFixed(2);
+    if (age.value === "minorenne") {
+        return discountForMinor(basePrice);
     }
-    if(age.value === "maggiorenne"){
-       return basePrice;
+    if (age.value === "maggiorenne") {
+        return basePrice;
     }
-    if(age.value === "over65"){
-       return discountForOver65(basePrice);
-    }    
+    if (age.value === "over65") {
+        return discountForOver65(basePrice);
+    }
 }
-function discountForMinor(basePrice){
-   const discount = (basePrice * 20 / 100).toFixed(2);
-   const discountedPrice = (basePrice - discount).toFixed(2);
-    return discountedPrice
+function discountForMinor(basePrice) {
+    const discount = (basePrice * 20 / 100).toFixed(2);
+    const discountedPrice = (basePrice - discount).toFixed(2);
+    return discountedPrice;
 }
 
-function discountForOver65(basePrice){
+function discountForOver65(basePrice) {
     const discount = (basePrice * 40 / 100).toFixed(2);
     const discountedPrice = (basePrice - discount).toFixed(2);
+    return discountedPrice;
 }
