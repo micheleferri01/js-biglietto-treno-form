999d36cc0f89653cb69821e4bd3fb9d87f4a073f
micheleferri01 <micheleferri095@gmail.com>
2026-01-13 16:45:29 +0100

    milestone 1

--- javascript/main.js
+++ javascript/main.js
@@ -4,12 +4,35 @@ const age = document.getElementById("age");
 
 ticketForm.addEventListener("submit", (e) =>{
     e.preventDefault();
-    const pricePerKil = 0.21;
-    const basePrice = (pricePerKil * kil.value).toFixed(2);
     console.log(`Kilometri da percorrere: ${kil.value} Km`);
     console.log("Età:",age.value);
-    console.log(`Prezzo: ${basePrice} €`);
+    const priceForUser = price();
+    console.log(`Prezzo Biglietto: ${priceForUser} €`)
     kil.value = "";
     age.value = "";
-
+    
 });
+
+function price(){
+    const pricePerKil = 0.21;
+    const basePrice = (pricePerKil * kil.value).toFixed(2);
+    if(age.value === "minorenne"){
+       return discountForMinor(basePrice);
+    }
+    if(age.value === "maggiorenne"){
+       return basePrice;
+    }
+    if(age.value === "over65"){
+       return discountForOver65(basePrice);
+    }    
+}
+function discountForMinor(basePrice){
+   const discount = (basePrice * 20 / 100).toFixed(2);
+   const discountedPrice = (basePrice - discount).toFixed(2);
+    return discountedPrice
+}
+
+function discountForOver65(basePrice){
+    const discount = (basePrice * 40 / 100).toFixed(2);
+    const discountedPrice = (basePrice - discount).toFixed(2);
+}
