46ec0e9467231357d840e5d26282502437b9eb8d
micheleferri01 <micheleferri095@gmail.com>
2026-01-14 11:18:36 +0100

    aggiunto input per nome passeggero

--- javascript/main.js
+++ javascript/main.js
@@ -1,31 +1,42 @@
 const ticketForm = document.getElementById("ticket-form");
+const nameInput = document.getElementById("name");
 const kilInput = document.getElementById("kilometri");
 const ageInput = document.getElementById("age");
 
 ticketForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    let kil = kilInput.value;
+    let name = nameInput.value;
+    let kil = parseInt(kilInput.value);
     let age = ageInput.value;
+    const ticket = document.getElementById("ticket");
+
+    console.log(`Nome passeggero: ${name}`);
     console.log(`Kilometri da percorrere: ${kil} Km`);
     console.log("Età:", age);
     const priceForUser = price();
     console.log(`Prezzo Biglietto: ${priceForUser} €`);
-    const ticket = document.getElementById("ticket");
+
+    const passengerName = document.getElementById("passenger-name");
     const Kilometri = document.getElementById("km");
     const ageCategory = document.getElementById("age-category");
     const finalPrice = document.getElementById("prezzo");
 
     if(!kil || !age){
+        passengerName.innerText = "";
         Kilometri.innerText = "";
         ageCategory.innerText = "";
         finalPrice.innerText = "";
+        ticket.classList.add("visually-hidden");
         return;
     }
 
+    passengerName.innerText = `${name}`;
     Kilometri.innerText = `${kil} Km`;
     ageCategory.innerText = `${age}`;
     finalPrice.innerText = `${priceForUser} €`;
+    ticket.classList.remove("visually-hidden");
 
+    nameInput.value = "";
     kilInput.value = "";
     ageInput.value = "";
 
